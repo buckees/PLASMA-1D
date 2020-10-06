ea84374cc7a407b2c76da46e62589f4648eeb229
buckees <67809187+buckees@users.noreply.github.com>
2020-10-06 16:26:41 -0700

    Remove unnecesary

--- CCP1d.py
+++ CCP1d.py
@@ -105,30 +105,6 @@ class CCP_1d(object):
         # show fig
         plt.show(fig)
 
-    def add_bndy(self):
-        """Add boundaries."""
-        pass
-
-    def add_mat(self):
-        """Add materials."""
-        pass
-
-    def cnt_diff(self, y):
-        """
-        Caculate dy/dx using central differencing.
-
-        Using x[i+1] - x[i-1] instead of delx makes cnt_diff() compatible
-        with non-uniform 1d mesh.
-        """
-        dy = np.zeros_like(self.x)
-        # Although dy[0] and dy[-1] are signed here,
-        # they are eventually specified in boundary conditions
-        dy[0] = (y[1] - y[0])/(self.x[1] - self.x[0])
-        dy[-1] = (y[-1] - y[-2])/(self.x[-1] - self.x[-2])
-        for i in range(1, self.nx-1):
-            dy[i] = (y[i+1] - y[i-1])/(self.x[i+1] - self.x[i-1])
-        return dy
-
 
 if __name__ == '__main__':
     """Test Mesh."""
@@ -139,4 +115,3 @@ if __name__ == '__main__':
     ccp1d.plot_plasma()
     ccp1d.init_pot()
     ccp1d.plot_pot()
-
