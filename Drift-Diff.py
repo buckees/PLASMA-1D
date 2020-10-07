41edb9ed02da55eb0a5cb34c75f978f209277ef7
buckees <67809187+buckees@users.noreply.github.com>
2020-10-06 20:27:06 -0700

    Add DD_Base
    1. Add DD_Base as parent class for Ambipolar and Drift-Diff

--- Drift-Diff.py
+++ Drift-Diff.py
@@ -9,6 +9,60 @@ Output: plasma flux
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+class DD_Base():
+    """Define the base class for Drift-Diffusion and Ambipolar."""
+
+    def __init__(self, geom):
+        """Import geometry information."""
+        self.geom = geom
+        nx = self.geom.nx
+        self.fluxe = np.zeros(nx)  # initial eon flux
+        self.fluxi = np.zeros(nx)  # initial ion flux
+
+    def __str__(self):
+        """Print Drift-Diffusion Approximation."""
+        return f'label = {self.fluxe}'
+
+    def init_transp(self, De=5e-1, Di=5e-3, Mue=1.0, Mui=1e-4):
+        """
+        Initiate diffusion coefficient and mobility.
+
+        initial De=5e-1, Di=5e-3, in m^2/s
+        initial Mue=1.0, Mui=1e-4 in (m/s)*(m/V)
+        """
+        nx = self.geom.nx
+        self.De = np.ones(nx)*De  # initial eon diff coeff
+        self.Di = np.ones_like(self.De)*Di  # initial ion diff coeff
+        self.Mue = np.ones(nx)*Mue  # initial eon mobility
+        self.Mui = np.ones_like(self.Mue)*Mui  # initial ion mobility
+
+    def plot_transp(self):
+        """Plot potential, E-field."""
+        x = self.geom.x
+        fig, axes = plt.subplots(1, 2, figsize=(8, 4),
+                                 constrained_layout=True)
+        # plot potential
+        ax = axes[0]
+        ax.plot(x, self.De, 'b-')
+        ax.plot(x, self.Di, 'r-')
+        ax.legend(['e Diffusion Coeff', 'Ion Diffusion Coeff'])
+        # plot E-field
+        ax = axes[1]
+        ax.plot(x, self.Mue, 'b-')
+        ax.plot(x, self.Mui, 'r-')
+        ax.legend(['e Mobility', 'Ion Mobility'])
+        # show fig
+        plt.show(fig)
+
+    def calc_diff(self, CCP_1d):
+        """Calc diffusion term in flux."""
+        dnedx = self.geom.cnt_diff(CCP_1d.ne)
+        self.fluxe = self.De*dnedx
+        dnidx = self.geom.cnt_diff(CCP_1d.ni)
+        self.fluxi = self.Di*dnidx
 
 
 class Drift_Diff(object):
@@ -35,20 +89,13 @@ class Drift_Diff(object):
         """
         # self.De =
         # self.Di =
+        return self.De, self.Di
 
     def calc_flux(self, CCP_1d):
         """
         Calc plasma flux.
 
         The drift-diffusion approximation:
-            1. steady state, dne/dt = 0. it cannot be used to
-            describe plasma decay.
-            2. ni is calculated from continuity equation.
-            3. plasma is charge neutral, ne = ni
-            4. Ionization Se is needed to balance diffusion loss.
-        Da = Di + De*Mui/Mue
-        Da = Di(1 + Te/Ti).
-        Es = (Di - De)/(Mui + Mue)*dn/dx/n
         """
         self.Da = CCP_1d.Di*(1 + np.divide(CCP_1d.Te, CCP_1d.Ti))
         self.Ea = (CCP_1d.Di - CCP_1d.De)/(CCP_1d.Mui + CCP_1d.Mue)
