f87a4221eba0db6d4eb5a5e8479ff89e89a6a832
buckees <67809187+buckees@users.noreply.github.com>
2020-11-17 21:41:20 -0800

    Update RctMod1d_Geom.py

--- RctMod1d_Geom.py
+++ RctMod1d_Geom.py
@@ -76,27 +76,123 @@ class Segment(Interval):
         posn: unit in m, (2, ) array, position as input
         boundaries are not consindered as "Inside"
         """
-        return all(self.bl <= posn) and all(posn <= self.ur)
+        return self.lr[0] <= posn and posn <= self.lr[1]
 
-class Geom_1d(Domain_1d):
-    """Define 1d Geometry."""
+class Geom1d():
+    """Constuct the 1D geometry."""
+    
+    def __init__(self, name='Geometry', is_cyl=False):
+        """
+        Init the 1D geometry.
+        
+        dim = 1, this class only supports 1D geometry
+        is_cyl: bool, wether the geometry is cylidrical symmetric or not
+        """
+        self.name = name
+        self.dim = 1
+        self.is_cyl = is_cyl
+        self.num_mat = 0
+        self.label = None
+        self.sequence = list()
 
-    def __init__(self, label, width):
-        super().__init__(label, width)
+    def __str__(self):
+        """Print Geometry info."""
+        res = f'Geometry dimension {self.dim}D'
+        if self.is_cyl:
+            res += ' cylindrical'
+        res += '\nGeometry sequence:'
+        for shape in self.sequence:
+            res += '\n' + str(shape)
+        return res
 
-    def add_intv(self):
-        """Add intervals."""
-        pass
+    def add_domain(self, domain):
+        """
+        Add domain to the geometry.
+        
+        domain: class
+        lr: unit in m, (2, ) tuple, bndy of the domain
+        label: dict, label <-> number
+        """
+        self.lr = domain.lr
+        self.label ={'Plasma':0}
+        self.num_mat = 1
 
-    def add_bndy(self):
-        """Add boundaries."""
-        pass
+    def add_segment(self, segment):
+        """
+        Add segment to 1D geometry.
+        
+        segment: class
+        """
+        if self.num_mat:
+            self.sequence.append(segment)
+            if segment.label in self.label:
+                pass
+            else:
+                self.num_mat += 1
+                self.label[segment.label] = self.num_mat - 1
+        else:
+            res = 'Domian is not added yet.'
+            res += '\nRun self.add_domain() before self.add_shape()'
+            return res
 
-    def add_mat(self):
-        """Add materials."""
-        pass
+    def get_label(self, posn):
+        """
+        Return the label of a position.
+        
+        posn: unit in m, var, position as input
+        label: str, var, label of the segment
+        """
+        # what if return None
+        label = 'Plasma'
+        for segment in self.sequence:
+            if posn in segment:
+                label = segment.label
+        return label, self.label[label]
 
+    def label_check(self, posn, label):
+        """
+        Check if labelf of posn == label of input.
+        
+        posn: unit in m, var or (2, ) array, position as input
+        label: str, var, label as input
+        """
+        # cannot determined if the posn is in domain
+        res = False
+        posn_label = self.get_label(posn)
+        return res or (posn_label == label)
+    
+    def plot(self, figsize=(8, 8), dpi=300, fname='Geometry'):
+        """
+        Plot the 1D geometry.
+        
+        figsize: unit in inch, (2, ) tuple, determine the fig/canvas size
+        dpi: dimless, int, Dots Per Inch
+        """
+        color_dict = {0:'white', 1:'black', 2:'green', 3:'yellow', 
+                      4:'blue', 5:'pink', 6:'purple'}
+        
+        fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
+                                     constrained_layout=True)
+        
+        ax = axes[0]
+        
+        for segment in self.sequence:
+            temp_col = color_dict[self.label[segment.label]]
+            ax.plot(segment.lr[0], segment.lr[1],
+                    linewidth=5, color=temp_col)
+            
+        ax = axes[1]
+        ax.plot(self.domain.lr[0], self.domain.lr[1],
+                linewidth=5, color='purple')
+        for segment in self.sequence:
+            ax.plot(segment.lr[0], segment.lr[1],
+                linewidth=5, color='grey')
+        # for ax in axes:
+        #     ax.set_xlim(self.bl[0], self.bl[0] + self.domain[0])
+        #     ax.set_ylim(self.bl[1], self.bl[1] + self.domain[1])
+        fig.savefig(fname, dpi=dpi)
+        plt.close()
 if __name__ == '__main__':
-    """Test Geometry."""
+    """Test 1D Geometry."""
     geom1d = Geom_1d('A', 10e-2)
     print(geom1d)
